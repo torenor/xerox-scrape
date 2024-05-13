3c7711df637a512db67b607a0fd766314e70b5af
torenor <torenor10@gmail.com>
2024-05-13 09:04:27 +0200

    dd

--- dato.js
+++
@@ -1,7 +0,0 @@
-var timeInMss = Date.now()
-var timeInMssgt = new Date().getTime()
-var d = new Date(dateString);
-//var d = new Date(jsonDate);
-console.log(timeInMss)
-console.log(timeInMssgt)
-console.log(d)
